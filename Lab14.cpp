f06dc7794dfeb2ed9d310da3324d6ccbcc752bfa
Grant Luo <hongzhiluo@Tnargs-Mac.local>
2024-09-29 15:57:52 -0700

    create Lab14.cpp

---
+++ Lab14.cpp
@@ -0,0 +1,28 @@
+/***************************************************************************
+* COMSC-210 | Lab 14 | Grant Luo
+* Editor: CLion
+* Compiler: Apple clang version 15.0.0
+*/
+#include <iostream>
+using namespace std;
+
+void print_id(string const& lab_desc)
+
+class Color
+{
+private:
+};
+
+
+
+
+
+void print_id(string const& lab_desc)
+{
+    cout << "Grant Luo\n";
+    cout << lab_desc << endl;
+    cout << "Editor: CLion\n";
+    cout << "Compiler: Apple clang version 15.0.0\n";
+    cout << "File: " << __FILE__ << "\n";
+    cout << "Compiled: " << __DATE__ << " at " << __TIME__ << "\n\n";
+}
