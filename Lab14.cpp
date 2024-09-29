3dd8263fe64ca4eabc5c0f0c8ff193a813df925d
Grant Luo <hongzhiluo@Tnargs-Mac.local>
2024-09-29 16:42:03 -0700

    Finished the program

--- Lab14.cpp
+++ Lab14.cpp
@@ -1,12 +1,12 @@
 /***************************************************************************
 * COMSC-210 | Lab 14 | Grant Luo
 * Editor: CLion
-* Compiler: Apple clang version 15.0.0
+* Compiler: Apple clang version 16.0.0
 */
 #include <iostream>
 using namespace std;
 
-void print_id(string const& lab_desc)
+void print_id(string const& lab_desc);
 
 class Color
 {
@@ -16,30 +16,61 @@ class Color
         // Setters
         void setRed(int r) { red = r; }
         void setGreen(int g) { green = g; }
-        void setBlu(int b) { blue = b; }
+        void setBlue(int b) { blue = b; }
 
         // Getters
         int getRed() const { return red; }
         int getGreen() const { return green; }
         int getBlue() const { return blue; }
 
-        // Other method(s)
+        // Print out RGB values of this Color object
         void print() const
         {
-            cout <<
+            cout << "Color RGB (" << red << ", " << green <<", " << blue << ")\n";
         }
 
 };
 
+int main()
+{
+    print_id("Lab 14: Color Class");
 
+    // Instantiate Color objects
+    Color color1;
+    color1.setRed(255);
+    color1.setGreen(0);
+    color1.setBlue(122);
+
+    Color color2;
+    color2.setRed(0);
+    color2.setGreen(255);
+    color2.setBlue(0);
+
+    Color color3;
+    color3.setRed(100);
+    color3.setGreen(100);
+    color3.setBlue(100);
+
+    // Print out the RBG values
+    cout << "Color 1: ";
+    color1.print();
+
+    cout << "Color 2: ";
+    color2.print();
+
+    cout << "Color 3: ";
+    color3.print();
+
+    cout << "\n";
+    return 0;
+}
 
 
 void print_id(string const& lab_desc)
 {
-    cout << "Grant Luo\n";
-    cout << lab_desc << endl;
+    cout << "COMSC210 | Grant Luo | " << lab_desc << "\n";
     cout << "Editor: CLion\n";
-    cout << "Compiler: Apple clang version 15.0.0\n";
+    cout << "Compiler: Apple clang version 16.0.0\n";
     cout << "File: " << __FILE__ << "\n";
     cout << "Compiled: " << __DATE__ << " at " << __TIME__ << "\n\n";
 }
