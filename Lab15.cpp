7f11957a7e90eae023492483d8bb6745e803e3c8
Grant Luo <hongzhiluo@Tnargs-Mac.local>
2024-09-29 17:37:02 -0700

    Working on main() right now

--- Lab15.cpp
+++ Lab15.cpp
@@ -26,6 +26,28 @@ class Movie
         // Print method that displays the object data
         void print()
         {
-            cout << "Movie: " << screenWriter << "\n";
+            cout << "Movie: " << title << '\n';
+            cout.width(4); cout << "Year released: " << yearReleased << '\n';
+            cout.width(4); cout << "Screenwriter: " << screenWriter << "\n\n";
         }
 };
+
+void print_id(string const& lab_desc);
+
+int main()
+{
+    print_id("Lab 15: Movie Class");
+
+    vector<Movie> movieList {};
+
+    return 0;
+}
+
+void print_id(string const& lab_desc)
+{
+    cout << "COMSC210 | Grant Luo | " << lab_desc << "\n";
+    cout << "Editor: CLion\n";
+    cout << "Compiler: Apple clang version 16.0.0\n";
+    cout << "File: " << __FILE__ << "\n";
+    cout << "Compiled: " << __DATE__ << " at " << __TIME__ << "\n\n";
+}
